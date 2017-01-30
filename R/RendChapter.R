25a81a04abdfa77cb47fccd3084c6981995d61f1
bjcochrane <cochrabj@miamioh.edu>
2017-01-30 13:27:17 -0500

    Minor updates

--- R/RendChapter.R
+++ R/RendChapter.R
@@ -1,5 +1,5 @@
 # Short script to rend chapters
-ch.title ="ProbabilityChapter" # Change as needed
+ch.title ="Equilibria" # Change as needed
 pth <-paste0("./RmdChapters/",ch.title)
 rendHtml(pth,"./html/Chapters")
 
